fe4fcf4664a2b1a4f5d1fd7b3a864e8701a9ae32
Dmytro Filin <dmitriy.fillin@gmail.com>
2023-11-10 13:20:44 +0100

    Fix repository functions and tests

--- src/repository/photos.py
+++ src/repository/photos.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from sqlalchemy.future import select
+from sqlalchemy import and_
 from src.models.photo import Photo, Tag
 from src.schemas.photo import PhotoCreate, PhotoUpdate
 
@@ -7,16 +7,21 @@ from src.schemas.photo import PhotoCreate, PhotoUpdate
 async def create_photo(photo_data: PhotoCreate, current_user, db: Session):
     """
     The create_photo function creates a new photo in the database.
+        Args:
+            photo_data (PhotoCreate): The data to create a new Photo object with.
+            current_user (User): The user who is creating the Photo object.
+            db (Session): A connection to the database, used for querying and committing changes.
 
-    :param photo_data: PhotoCreate: Pass the data from the request body to create a new photo
-    :param current_user: Get the user id of the current user
-    :param db: Session: Access the database
-    :return: A new photo object
+    :param photo_data: PhotoCreate: Create a new photo object
+    :param current_user: Get the user_id of the current user
+    :param db: Session: Pass the database session to the function
+    :return: A new_photo object
+    :doc-author: Trelent
     """
     photo_dump = photo_data.model_dump()
     list_tags = db.query(Tag).filter(Tag.name.in_(photo_dump["tags"])).all()
     if len(list_tags) == 0:
-        list_tags = [Tag(name = i) for i in photo_dump["tags"]]
+        list_tags = [Tag(name=i) for i in photo_dump["tags"]]
     new_photo = Photo(
         image_url=photo_dump["image_url"],
         content=photo_dump["content"],
@@ -36,52 +41,54 @@ async def update_photo(
     The update_photo function updates a photo in the database.
         Args:
             photo_id (int): The id of the photo to update.
-            photo_data (PhotoUpdate): The data to update on the Photo object.
-            current_user: The user who is making this request, used for authorization purposes.
-            db (Session): A connection to our database, used for querying and updating records.
+            photo_data (PhotoUpdate): The data to update for the given Photo object.
+                This is a Pydantic model, so it will be validated before being passed into this function.
+            current_user: A FastAPI dependency that contains information about who is currently logged in and making this request.
+                It's used here to ensure that only users can edit their own photos, not other people's photos!
 
-    :param photo_id: int: Identify which photo to update
-    :param photo_data: PhotoUpdate: Pass in the data that will be used to update the photo
-    :param current_user: Check if the user is authorized to update a photo
+    :param photo_id: int: Find the photo in the database
+    :param photo_data: PhotoUpdate: Pass in the data from the request body
+    :param current_user: Check if the user is authorized to update the photo
     :param db: Session: Pass the database session to the function
     :return: The updated photo
     :doc-author: Trelent
     """
-    query = select(Photo).filter_by(id=photo_id, user_id=current_user.id)
-    result = await db.execute(query)
-    photo = result.scalars().first()
-    if photo:
+    db_photo = (
+        db.query(Photo)
+        .filter(and_(Photo.id == photo_id, Photo.user_id == current_user))
+        .first()
+    )
+    if db_photo:
         for var, value in vars(photo_data).items():
-            setattr(photo, var, value) if value is not None else None
-        photo.upd
-        await db.commit()
-        await db.refresh(photo)
-        return photo
-    return None
+            setattr(db_photo, var, value) if value is not None else None
+        db.commit()
+        db.refresh(db_photo)
+    return db_photo
 
 
 async def delete_photo(photo_id: int, current_user, db: Session):
     """
     The delete_photo function deletes a photo from the database.
         Args:
-            photo_id (int): The id of the photo to delete.
-            current_user (User): The user who is deleting the photo.
-            db (Session): A connection to our database session, used for querying and committing changes.
+            photo_id (int): The id of the photo to be deleted.
+            current_user (str): The username of the user who is deleting this photo.
+            db (Session): A connection to our database, used for querying and committing changes.
 
-    :param photo_id: int: Get the photo with that id from the database
+    :param photo_id: int: Identify the photo to be deleted
     :param current_user: Check if the user is authorized to delete the photo
     :param db: Session: Pass the database session to the function
     :return: A photo object
     :doc-author: Trelent
     """
-    query = select(Photo).filter_by(id=photo_id, user_id=current_user.id)
-    result = await db.execute(query)
-    photo = result.scalars().first()
-    if photo:
-        await db.delete(photo)
-        await db.commit()
-        return photo
-    return None
+    db_photo = (
+        db.query(Photo)
+        .filter(and_(Photo.id == photo_id, Photo.user_id == current_user))
+        .first()
+    )
+    if db_photo:
+        db.delete(db_photo)
+        db.commit()
+    return db_photo
 
 
 async def get_photo(photo_id: int, db: Session):
@@ -93,32 +100,26 @@ async def get_photo(photo_id: int, db: Session):
         Returns:
             Photo: The requested Photo object.
 
-    :param photo_id: int: Filter the query by id
-    :param db: Session: Pass the database session to the function
-    :return: A single photo
+    :param photo_id: int: Specify the id of the photo to be retrieved
+    :param db: Session: Pass the database session into the function
+    :return: A photo object
     :doc-author: Trelent
     """
-    query = select(Photo).filter_by(id=photo_id)
-    result = await db.execute(query)
-    photo = result.scalars().first()
-    return photo
+    return db.query(Photo).filter(and_(Photo.id == photo_id)).first()
 
 
-async def get_photos(current_user, db: Session):
+async def get_photos(skip: int, limit: int, current_user, db: Session):
     """
-    The get_photos function returns a list of photos for the current user.
-        Args:
-            current_user (User): The currently logged in user.
-            db (Session): A database session object to execute queries against.
-        Returns:
-            List[Photo]: A list of Photo objects that belong to the current user.
-
+    The get_photos function returns a list of photos from the database.
+        
+    
+    :param skip: int: Skip the first n photos
+    :param limit: int: Limit the number of photos returned
     :param current_user: Get the photos of a specific user
-    :param db: Session: Pass in the database session object
-    :return: A list of photos
+    :param db: Session: Pass the database session to the function
+    :return: A list of photo objects
     :doc-author: Trelent
     """
-    query = select(Photo).filter_by(user_id=current_user.id)
-    result = await db.execute(query)
-    photos = result.scalars().all()
-    return photos
+    return db.query(Photo).filter(
+        Photo.user_id == current_user.offset(skip).limit(limit).all()
+    )
