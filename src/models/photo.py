2542bbe517b09b54f01dbe0361a97f8d02596cb9
Dmytro Klymenko <119627462+leegosx@users.noreply.github.com>
2023-11-14 16:55:02 +0100

    Revert "Fix variables and models"

--- src/models/photo.py
+++ src/models/photo.py
@@ -3,28 +3,27 @@ from sqlalchemy.orm import relationship
 
 from src.models.base import BaseModel, Base
 
-image_m2m_tags = Table(
-    "image_m2m_tags",
+photo_m2m_tags = Table(
+    "photo_m2m_tags",
     Base.metadata,
     Column("id", Integer, primary_key=True),
-    Column("image_id", Integer, ForeignKey("images.id", ondelete="CASCADE")),
+    Column("photo_id", Integer, ForeignKey("photos.id", ondelete="CASCADE")),
     Column("tag_id", Integer, ForeignKey("tags.id", ondelete="CASCADE")),
 )
 
 
-class Image(BaseModel):
-    __tablename__ = "images"
+class Photo(BaseModel):
+    __tablename__ = "photos"
 
     image_url = Column(String, nullable=False)
-    image_transformed_url = Column(String, nullable=True)
     content = Column(String, nullable=False)
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
-    user = relationship("User", back_populates="images")
-    tags = relationship("Tag", secondary=image_m2m_tags, back_populates="images")
+    user = relationship("User", back_populates="photos")
+    tags = relationship("Tag", secondary=photo_m2m_tags, back_populates="photos")
 
 
 class Tag(BaseModel):
     __tablename__ = "tags"
 
     name = Column(String, unique=True)
-    images = relationship("Image", secondary=image_m2m_tags, back_populates="tags")
+    photos = relationship("Photo", secondary=photo_m2m_tags, back_populates="tags")
--- src/repository/users.py
+++ src/repository/users.py
@@ -8,16 +8,6 @@ from src.schemas.user import UserBase
 
 
 async def get_user_by_email(email: str, db: Session) -> Optional[User]:
-    """
-    The get_user_by_email function takes in an email and a database session,
-    and returns the user associated with that email. If no such user exists,
-    it returns None.
-    
-    :param email: str: Pass in the email of the user we want to retrieve from our database
-    :param db: Session: Pass the database session to the function
-    :return: The first user with the specified email
-    :doc-author: Trelent
-    """
     return db.query(User).filter(User.email == email).first()
 
 
@@ -49,49 +39,17 @@ async def create_user(body: UserBase, db: Session) -> User:
 
 
 async def update_token(user: UserBase, token: Union[str, None], db: Session) -> None:
-    """
-    The update_token function updates the refresh token for a user.
-    
-    :param user: UserBase: Identify the user in the database
-    :param token: Union[str: Pass the token to the function
-    :param None]: Indicate that the function can accept either a string or none
-    :param db: Session: Commit the changes to the database
-    :return: None
-    :doc-author: Trelent
-    """
     user.refresh_token = token
     db.commit()
 
 
 async def confirmed_email(email: str, db: Session) -> None:
-    """
-    The confirmed_email function sets the confirmed field of a user to True.
-    
-    :param email: str: Get the email of the user to confirm
-    :param db: Session: Pass the database session to the function
-    :return: None
-    :doc-author: Trelent
-    """
     user = await get_user_by_email(email, db)
     user.confirmed = True
     db.commit()
 
 
 async def update_avatar(email, url: str, db: Session) -> User:
-    """
-    The update_avatar function updates the avatar of a user.
-    
-    Args:
-        email (str): The email address of the user to update.
-        url (str): The URL for the new avatar image.
-        db (Session, optional): A database session object to use instead of creating one locally. Defaults to None.  # noQA: E501 line too long; pylint: disable=line-too-long  # noQA: E501 line too long; pylint: disable=line-too-long  # noQA: E501 line too long;
-    
-    :param email: Get the user from the database
-    :param url: str: Specify the type of data that will be passed into the function
-    :param db: Session: Pass in the database session to the function
-    :return: A user object
-    :doc-author: Trelent
-    """
     user = await get_user_by_email(email, db)
     user.avatar = url
     db.commit()
