2452c451dc7879ced4cdba53b207d281ff9996ac
Adentas <adentaspro@gmail.com>
2023-11-14 04:03:30 +0200

    comments fix

--- src/repository/comments.py
+++ src/repository/comments.py
@@ -1,34 +1,41 @@
 from typing import List
+from fastapi import HTTPException
 from sqlalchemy.orm import Session
 from sqlalchemy import and_
 from src.models.comment import Comment
 from src.schemas.comment import CommentRequest
 
-class CommentRepository:
-    def create_comment(self, db: Session, comment: CommentRequest, user_id: int, photo_id: int) -> Comment:
-        db_comment = Comment(**comment.dict(), user_id=user_id, photo_id=photo_id)
-        db.add(db_comment)
-        db.commit()
-        db.refresh(db_comment)
-        return db_comment
+async def create_comment(db: Session, comment: CommentRequest, user_id: int, photo_id: int) -> Comment:
+    db_comment = Comment(**comment.dict(), user_id=user_id, photo_id=photo_id)
+    db.add(db_comment)
+    await db.commit()
+    await db.refresh(db_comment)
+    print('Create')
+    return db_comment
 
-    def get_comment_by_id(self, db: Session, comment_id: int, user_id: int) -> Comment:
-        return db.query(Comment).filter(and_(Comment.id == comment_id, Comment.user_id == user_id)).first()
+async def get_comment_by_id(db: Session, comment_id: int, user_id: int) -> Comment:
+    if comment_id is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Comment id is required')
+    print('get')
+    return db.query(Comment).filter(and_(Comment.id == comment_id, Comment.user_id == user_id)).first()
 
-    def update_comment(self, db: Session, comment_id: int, new_content: str, user_id: int) -> Comment:
-        comment = self.get_comment_by_id(db, comment_id, user_id)
-        if comment:
-            comment.content = new_content
-            db.commit()
-            db.refresh(comment)
-        return comment
+async def update_comment(db: Session, comment_id: int, new_content: str, user_id: int) -> Comment:
+    comment = await get_comment_by_id(db, comment_id, user_id)
+    print('update')
+    if comment is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Comment not found')
+    if comment:
+        comment.content = new_content
+        await db.commit()
+        await db.refresh(comment)
+    return comment
 
-    def delete_comment(self, db: Session, comment_id: int, user_id: int) -> Comment:
-        comment = self.get_comment_by_id(db, comment_id, user_id)
-        if comment:
-            db.delete(comment)
-            db.commit()
-        return comment
-
-    def get_comments_for_photo(self, db: Session, photo_id: int) -> List[Comment]:
-        return db.query(Comment).filter(Comment.photo_id == photo_id).all()
+async def delete_comment(db: Session, comment_id: int, user_id: int) -> Comment:
+    comment = await get_comment_by_id(db, comment_id, user_id)
+    print('delete')
+    if comment_id is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Comment id is required')
+    if comment:
+        db.delete(comment)
+        await db.commit()
+    return comment
--- src/routes/comments.py
+++ src/routes/comments.py
@@ -1,22 +1,32 @@
 from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
 from src.schemas.comment import CommentRequest, CommentResponse
-from src.database import get_db
-from src.repositories.comment import CommentRepository
+from src.database.db import get_db
+from src.repository.comments import (
+    create_comment,
+    get_comment_by_id,
+    update_comment,
+    delete_comment
+)
 
 router = APIRouter()
-comment_repository = CommentRepository()
 
 @router.post("/comments/", response_model=CommentResponse)
 async def create_comment(comment_req: CommentRequest, db: Session = Depends(get_db)):
     # Операція створення коментаря
-    comment = comment_repository.create_comment(db, comment_req, user_id=1, photo_id=1)  # Потрібно вказати конкретні user_id та photo_id
+    # user_id та photo_id можна отримати з контексту аутентифікації, якщо потрібно
+    user_id = 1  # Приклад фіксованого значення user_id
+    photo_id = 1  # Приклад фіксованого значення photo_id
+    comment = await create_comment(db, comment_req, user_id=user_id, photo_id=photo_id)
     return comment
 
 @router.get("/comments/{comment_id}", response_model=CommentResponse)
 async def get_comment(comment_id: int, db: Session = Depends(get_db)):
     # Отримання коментаря за його ідентифікатором
-    comment = comment_repository.get_comment_by_id(db, comment_id, user_id=1)  # Потрібно вказати конкретний user_id
+    if comment_id is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Comment id is required')
+    user_id = 1  # Приклад фіксованого значення user_id
+    comment = await get_comment_by_id(db, comment_id, user_id=user_id)
     if comment is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Comment not found")
     return comment
@@ -24,7 +34,10 @@ async def get_comment(comment_id: int, db: Session = Depends(get_db)):
 @router.put("/comments/{comment_id}", response_model=CommentResponse)
 async def update_comment(comment_id: int, comment_req: CommentRequest, db: Session = Depends(get_db)):
     # Операція оновлення коментаря
-    comment = comment_repository.update_comment(db, comment_id, comment_req.content, user_id=1)  # Потрібно вказати конкретний user_id
+    if comment_id is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Comment id is required')
+    user_id = 1  # Приклад фіксованого значення user_id
+    comment = await update_comment(db, comment_id, comment_req.content, user_id=user_id)
     if comment is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Comment not found")
     return comment
@@ -32,7 +45,10 @@ async def update_comment(comment_id: int, comment_req: CommentRequest, db: Sessi
 @router.delete("/comments/{comment_id}", response_model=CommentResponse)
 async def delete_comment(comment_id: int, db: Session = Depends(get_db)):
     # Операція видалення коментаря
-    comment = comment_repository.delete_comment(db, comment_id, user_id=1)  # Потрібно вказати конкретний user_id
+    if comment_id is None:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='Comment id is required')
+    user_id = 1  # Приклад фіксованого значення user_id
+    comment = await delete_comment(db, comment_id, user_id=user_id)
     if comment is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Comment not found")
     return comment
