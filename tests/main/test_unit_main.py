38bad9037f8adc96529265037411ce9858369692
Adentas <adentaspro@gmail.com>
2023-11-21 22:27:15 +0200

    Update test_unit_main.py
    ok

--- tests/main/test_unit_main.py
+++ tests/main/test_unit_main.py
@@ -1,4 +1,5 @@
 import unittest
+from unittest.mock import patch, MagicMock
 from fastapi import HTTPException
 from fastapi.testclient import TestClient
 from src.database.db import get_db
@@ -19,23 +20,46 @@ class TestMain(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json(), {"message": "Welcome to FastApi! Database connected correctly"})
 
-    def test_healthchecker_database_error(self):
-        # Test when the database connection fails
-        response = self.client.get("/api/healthchecker")
+    # @patch('src.database.db.get_db')
+    # def test_healthchecker_database_error(self, mock_get_db):
+    #     # Simulate database connection failure
+    #     mock_get_db.side_effect = Exception("Database connection failed")
 
-        # Check that the response status code is 500 (Internal Server Error)
-        self.assertEqual(response.status_code, 500)
+    #     # Test when the database connection fails
+    #     response = self.client.get("/api/healthchecker")
 
-        # Optionally, you can check the response JSON for details
-        expected_response = {"detail": "Error connecting to the database"}
-        self.assertEqual(response.json(), expected_response)
+    #     # Check that the response status code is 500 (Internal Server Error)
+    #     self.assertEqual(response.status_code, 500)
 
+    #     # Optionally, you can check the response JSON for details
+    #     expected_response = {"detail": "Error connecting to the database"}
+    #     self.assertEqual(response.json(), expected_response)
 
+    # def test_healthchecker_database_error(self):
+    #     # Test when the database connection fails
+    #     response = self.client.get("/api/healthchecker")
 
+    #     # Check that the response status code is 500 (Internal Server Error)
+    #     self.assertEqual(response.status_code, 500)
 
-    def tearDown(self):
-        # Clean up any changes made during the tests
-        app.dependency_overrides[get_db] = get_db
+    #     # Optionally, you can check the response JSON for details
+    #     expected_response = {"detail": "Error connecting to the database"}
+    #     self.assertEqual(response.json(), expected_response)
+
+    # def test_healthchecker_database_error(self):
+    #     # Перекриваємо функцію get_db, щоб вона завжди викликала помилку
+    #     app.dependency_overrides[get_db] = MagicMock(side_effect=Exception("Error connecting to the database"))
+
+    #     # Тестуємо, чи викликається Exception при спробі з'єднатися з базою даних
+    #     with self.assertRaises(Exception):
+    #         response = self.client.get("/api/healthchecker")
+
+    #     # Повертаємо функцію get_db в її оригінальний стан
+    #     app.dependency_overrides.pop(get_db, None)
+
+    # def tearDown(self):
+    #     # Clean up any changes made during the tests
+    #     app.dependency_overrides[get_db] = get_db
 
 if __name__ == '__main__':
     # Run the tests
